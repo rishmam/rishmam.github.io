6a59567603678a33597a4962977a8ce5ab5abfdf
rishmamurugan <rishma2000@gmail.com>
2020-02-01 15:06:34 -0800

    Firebase Config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import firebaseConfig from "config.js";
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
@@ -5,8 +6,8 @@ import firebaseConfig from firebaseConfig.js;
 //console.log(document.getElementById("myText").value);
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-        apiKey: "AIzaSyCvpEisytGcXdAqjyxrWiDrAvqy86Bi6Dk",
-        authDomain:"bearp-493dc.firebaseapp.com",
-        projectId: "bearp-493dc"
+        apiKey: firebaseConfig.apiKey,
+        authDomain: firebaseConfig.authDomain,
+        projectId: firebaseConfig.projectId
     });
     
